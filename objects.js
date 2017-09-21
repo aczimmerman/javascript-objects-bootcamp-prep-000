575b9e75fdc1077642f99bb113801035245974b2
Andrea Zimmerman <andrea.c.zimmerman@gmail.com>
2017-09-21 22:08:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,13 @@
-var playlist = {artistName: 'Song Title'}
+var playlist = {
+  Beyonce: "Crazy in Love",
+  Prince: "Purple Rain"
 
-function updatePlaylist(obj, artistName, songTitle) {
-  return Object.assign({}, obj, {[artistName]: songTitle)
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
 }
